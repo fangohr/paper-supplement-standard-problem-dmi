986565e9c5cb0fae8917abb6b6f00bc2d3c1c36c
davidcorteso <d.i.cortes@soton.ac.uk>
2018-03-01 15:11:48 +0000

    Added Finmag simulations

---
+++ sims/finmag/2D/nanodisk_T.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+from finmag import Simulation as Sim
+from finmag.energies import Exchange, Demag, DMI, Zeeman
+from finmag.energies import UniaxialAnisotropy
+from finmag.util.consts import mu0
+
+import os, shutil
+import dolfin as df
+
+# Info
+# from finmag.util.helpers import set_logging_level
+# set_logging_level("INFO")
+# set_logging_level("DEBUG")
+
+# Geometries
+from finmag.util.mesh_templates import Nanodisk
+
+
+# MESH ------------------------------------------------------------------------
+
+mesh_file = 'mesh/nanodisk.xml.gz'
+if os.path.exists(mesh_file):
+    mesh = df.Mesh(mesh_file)
+else:
+    mesh = Nanodisk(d=100, h=2,
+                    center=(0, 0, 0),
+                    valign='bottom',
+                    name='nanodisk'
+                    ).create_mesh(maxh=2,
+                                  save_result=True,
+                                  filename='nanodisk', directory='mesh',
+                                  )
+
+
+# Simulation and energies -----------------------------------------------------
+
+# Bulk
+A = 13e-12
+D = 3e-3
+Ms = 0.86e6
+Ku = 0.4e6
+initial_sk_diam = 10
+
+sim = Sim(mesh, Ms=Ms, unit_length=1e-9, name="nanodisk_T")
+
+
+# A linear model for the skyrmion profile
+def m_init(pos):
+    x, y = pos[0], pos[1]
+    if (x ** 2 + y ** 2) ** 0.5 < initial_sk_diam:
+        r = (x ** 2 + y ** 2) ** 0.5
+        phi = np.arctan2(y, x) + 0.5 * np.pi
+        k = np.pi / initial_sk_diam
+        return (np.sin(k * r) * np.cos(phi),
+                np.sin(k * r) * np.sin(phi),
+                -np.cos(k * r))
+    else:
+        return (0, 0, 1)
+
+sim.set_m(m_init)
+
+# -----------------------------------------------------------------------------
+
+# Exchange Energy
+sim.add(Exchange(A))
+# DMI
+sim.add(DMI(D, dmi_type='bulk'))
+# Uniaxial Anisotropy
+sim.add(UniaxialAnisotropy(Ku, (0, 0, 1), name='Ku'))
+
+# -----------------------------------------------------------------------------
+
+sim.do_precession = False
+sim.alpha = 0.9
+
+if not os.path.exists('vtks'):
+    # shutil.rmtree('vtks')
+    os.mkdir('vtks')
+
+sim.relax()
+sim.save_vtk(filename='vtks/nanodisk_T.pvd', overwrite=True)
+sim.save_field('m', 'nanodisk_T.npy', overwrite=True)
---
+++ sims/finmag/3D/retrieve_data.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+from finmag import Simulation as Sim
+from finmag.energies import Exchange, Demag, DMI, Zeeman
+from finmag.util.consts import mu0
+
+import os, shutil
+import dolfin as df
+
+import scipy.optimize
+
+import finmag
+
+
+# MESH ------------------------------------------------------------------------
+
+mesh_file = 'mesh/nanocylinder.xml.gz'
+mesh = df.Mesh(mesh_file)
+
+# Simulation and energies -----------------------------------------------------
+
+# Bulk
+A = 8.78e-12
+D = 1.58e-3
+Ms = 3.84e5
+B = 0.4
+initial_sk_diam = 20
+
+sim = Sim(mesh, Ms=Ms, unit_length=1e-9, name="unnamed")
+sim.set_m(np.load('m_relaxed.npy'))
+
+
+# -----------------------------------------------------------------------------
+# Skyrmion radius vs z
+
+sk_radius = {}
+for i, z in enumerate(np.linspace(0, 20, 11)):
+    # Skyrmion radius: m_z = 0
+    rsk = scipy.optimize.brentq(lambda x: sim.m_field.f((x, 0, z))[2], 0, 50)
+    sk_radius[z] = rsk
+    print('At z = {:<5}, r_sk = {:.3f}'.format(z, rsk))
+
+# -----------------------------------------------------------------------------
+# Get the data at the center of the cylinder
+
+data = {}
+
+xs = np.linspace(0, 89.9, 100)
+zs = [0, 10., 20.]
+for j, z_layer in enumerate(['bottom', 'center', 'top']):
+    data[z_layer] = np.zeros((len(xs), 8))
+    for i, x in enumerate(xs):
+
+        xi, yi, zi = x, 0, zs[j]
+        phi = np.arctan2(yi, xi)
+
+        mx, my, mz = np.copy(sim.m_field.f((xi, yi, zi)))
+        mphi = (-mx * np.sin(phi) + my * np.cos(phi))
+        mr = (mx * np.cos(phi) + my * np.sin(phi))
+
+        data[z_layer][i] = [xi, yi, zi, mx, my, mz, mr, mphi]
+
+    np.savetxt('m_{}.dat'.format(z_layer), data[z_layer])
+
+# -----------------------------------------------------------------------------
+# Extract m components across the thickness
+
+data_acrossz = {}
+zs = np.linspace(0, 20, 11)
+for j, x_pos in enumerate(['center', 'sk_rad', 'boundary']):
+    xs = [0, sk_radius[0], 89.9]
+    data_acrossz[x_pos] = np.zeros((len(zs), 8))
+    for i, z in enumerate(zs):
+
+        xi, yi, zi = xs[j], 0, z
+        phi = np.arctan2(yi, xi)
+
+        mx, my, mz = np.copy(sim.m_field.f((xi, yi, zi)))
+        mphi = (-mx * np.sin(phi) + my * np.cos(phi))
+        mr = (mx * np.cos(phi) + my * np.sin(phi))
+
+        data_acrossz[x_pos][i] = [xi, yi, zi, mx, my, mz, mr, mphi]
+
+    np.savetxt('m_across-z_{}.dat'.format(x_pos), data_acrossz[x_pos])
+
+
+# Print mesh
+print('Mesh\n')
+print(finmag.util.meshes.mesh_info(mesh))
+print(finmag.util.meshes.mesh_quality(mesh))
