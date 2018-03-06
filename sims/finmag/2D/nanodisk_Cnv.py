3d05a1c450c24b634c6c3a52f6483e282d1d790b
davidcorteso <d.i.cortes@soton.ac.uk>
2018-03-06 17:19:39 +0000

    Updated nbs. Trying to updated 2D sims

--- sims/finmag/2D/nanodisk_Cnv.py
+++ sims/finmag/2D/nanodisk_Cnv.py
@@ -35,7 +35,7 @@ else:
 
 # Simulation and energies -----------------------------------------------------
 
-# Bulk
+# Interfacial
 A = 13e-12
 D = 3e-3
 Ms = 0.86e6
@@ -45,18 +45,12 @@ initial_sk_diam = 10
 sim = Sim(mesh, Ms=Ms, unit_length=1e-9, name="nanodisk_Cnv")
 
 
-# A linear model for the skyrmion profile
 def m_init(pos):
-    x, y = pos[0], pos[1]
-    if (x ** 2 + y ** 2) ** 0.5 < initial_sk_diam:
-        r = (x ** 2 + y ** 2) ** 0.5
-        phi = np.arctan2(y, x) + 0.5 * np.pi
-        k = np.pi / initial_sk_diam
-        return (np.sin(k * r) * np.cos(phi),
-                np.sin(k * r) * np.sin(phi),
-                -np.cos(k * r))
+    x, y = np.array(pos)[:2]
+    if (x ** 2 + y ** 2) ** 0.5 < 25:
+        return (0, 0.1, 1)
     else:
-        return (0, 0, 1)
+        return (0, 0.1, -1)
 
 sim.set_m(m_init)
 
@@ -78,6 +72,40 @@ if not os.path.exists('vtks'):
     # shutil.rmtree('vtks')
     os.mkdir('vtks')
 
-sim.relax()
+sim.relax(stopping_dmdt=0.01)
 sim.save_vtk(filename='vtks/nanodisk_Cnv.pvd', overwrite=True)
 sim.save_field('m', 'nanodisk_Cnv.npy', overwrite=True)
+
+# Extract data
+r_diam = np.linspace(-49.99, 49.99, 100)
+data = np.zeros((len(r_diam), 4))
+
+for i, r in enumerate(r_diam):
+    data[i][0] = r
+    data[i][1:] = sim.m_field.f((r, 0, 1))
+
+np.savetxt('nanodisk_Cnv_skyrmion.dat', data)
+
+# Skyrmion radius
+import scipy.optimize
+r_sk = scipy.optimize.brentq(lambda r: sim.m_field.f((r, 0, 1))[2], 0, 49)
+print(r_sk)
+print(sim.m_field.f((r_sk, 0, 1)))
+
+# Extract the radial component at r=r_sk
+phi_ring = np.linspace(0, 2 * np.pi, 100)
+data_mr = np.zeros((len(phi_ring), 2))
+for i, phi in enumerate(phi_ring):
+    x = r_sk * np.cos(phi)
+    y = r_sk * np.sin(phi)
+
+    mx, my, mz = sim.m_field.f((x, y, 1))
+    print(x, y, mz)
+
+    mr = mx * np.cos(phi) + my * np.sin(phi)
+    mphi = -mx * np.sin(phi) + my * np.cos(phi)
+
+    data_mr[i][0] = phi
+    data_mr[i][1] = mr
+
+np.savetxt('nanodisk_Cnv_phi_mr_rsk.dat', data)
