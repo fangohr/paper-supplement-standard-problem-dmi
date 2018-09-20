7edd9f03634372ab493514755b9068ce86d78037
davidcorteso <d.i.cortes@soton.ac.uk>
2018-09-20 16:35:38 +0100

    Added OOMMF script to test boundaries

---
+++ sims/3D_OOMMF/convergence/makefile.py
@@ -0,0 +1,23 @@
+import subprocess
+
+discr = [(183, 150),
+         (182, 200),
+         (182.5, 250),
+         (184.25, 275),
+         (181, 100),
+         (180.5, 50),
+         ]
+
+for L, dxy in discr:
+
+    subprocess.call(('oommf '
+                     ' boxsi -threads 6 '
+                     '-parameters '
+                     '"'
+                     'L {0}e-9 '
+                     'dxy {1}e-11 '
+                     'BASENAME '
+                     '3D_skyrmion_d{1}e-2nm'
+                     '" '
+                     'oommf_3D_bulkDMI_cylinder.mif').format(L, dxy),
+                    shell=True)
